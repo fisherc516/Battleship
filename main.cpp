9530d8dac413dc5c1dcd6b73c36552a83c9e5b39
fisherc516 <c086f573@ku.edu>
2016-09-20 23:00:05 -0500

    Create main.cpp/brainstorm

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	//4 10x10 arrays full of zeros ------- Continually display guess board and player board
+	//Let user place ships
+	//Select random places for computer's ships
+	//Let user pick spot
+	//Computer picks random spot - if a hit, pick consecutive spot (depth first)
+	return(0);
+}

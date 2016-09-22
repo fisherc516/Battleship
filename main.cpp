11e6bcbbd0cab65c2b891c20a626c7496e1ac05d
fisherc516 <c086f573@ku.edu>
2016-09-21 19:50:39 -0500

    Initialize player + computer boards / Let player pick placement or pick random

--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 
 int main()
 {
-	//4 10x10 arrays full of zeros ------- Continually display guess board and player board
-	//Let user place ships
-	//Select random places for computer's ships
-	//Let user pick spot
-	//Computer picks random spot - if a hit, pick consecutive spot (depth first)
+	Board* computer = new Board();
+	Board* player = new Board();
+	computer->randomShips();
+	int choice = 0;
+	while(choice != 1 && choice != 2)
+	{
+		std::cout << "\nPlayer, do you want to...";
+		std::cout << "\n\t1) place your ships yourself?";
+		std::cout << "\n\t2) have your ships randomly placed?";
+		std::cout << "\n: ";
+		std::cin >> choice;
+		if(choice == 1)
+		{
+			player->pickShips();
+		}
+		else if(choice == 2)
+		{
+			player->randomShips();
+		}
+	}
+	//Let user guess
+	//Computer guesses random spot - if a hit, pick consecutive spot (depth first)
+	delete computer;
+	delete player;
 	return(0);
 }

489e8e74f083f2b8ca3a122fa1272f5f4caa0331
fisherc516 <c086f573@ku.edu>
2017-02-17 16:34:55 -0600

    Add gets for boards and special method for getting user coordinates

--- board.h
+++ board.h
@@ -5,19 +5,34 @@
 #include <cstdlib> /** srand(), rand() */
 #include <ctime> /** time() */
 #include <iostream>
+#include <string>
 
 class Board
 {
 	public:
+		// Creates two 2D arrays for tracking hits and misses	
 		Board();
+		// Deletes a board
+		~Board();
+		// Places ships randomly on shipBoard
 		void randomShips();
+		// Allows user to place ships manually
 		void pickShips();
-		void printBoards();
-		bool numbersChecker(std::string c);
+		// Illicits valid ship placement from user
+		void getCoordinates(int shipLength);
+		// Prints hitBoard(1)/shipBoard(2)/or both(3)
+		void printBoards(int n) const;
+		// Makes sure letters and numbers of coordinates are valid
+		bool coordinatesChecker(std::string c);
+		// Makes sure coordinates span correct number of spaces for each ship's length
 		bool spaceChecker(std::string c, int spaces);
+		// Returns shipBoard
+		char** getShipBoard();
+		// Returns hitBoard
+		char** getHitBoard();	
 	private:
-		int shipBoard [10][10];
-		char hitBoard [10][10];
+		char** shipBoard;
+		char** hitBoard;
 };
 
 #endif

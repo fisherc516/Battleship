bbf731ebb22a95e89b5dbfac8708199f8194910b
fisherc516 <c086f573@ku.edu>
2017-02-17 17:02:50 -0600

    Add guess validator and libraries for getting random guesses

--- game.h
+++ game.h
@@ -2,14 +2,22 @@
 #define GAME_H
 
 #include "board.h"
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 class Game
 {
 	public:
+		// Sets player and computer
 		Game(Board* p, Board* c);
+		// Lets player guess coordinates to attack; returns true if all computer's ships are sunk
 		bool playerGuess();
+		// Chooses random coordinates to attack; returns true if all player's ships are sunk
 		bool compGuess();
+		// Makes sure coordinate is valid
+		bool numbersChecker(std::string g);
 	private:
 		Board* player;
 		Board* computer;
